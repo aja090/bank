5c0a1564fc872c50f44abe185971bedf1622b88a
Aseel Alsalmasi <93863209+aja090@users.noreply.github.com>
2024-02-29 12:43:20 +0300

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from "./Billing"
+import Business from "./Business"
+import CardDeal from "./CardDeal"
+import Clients from './Clinets'
+import CTA from "./CTA"
+import FeedBackCard from "./FeedBackCard"
+import Footer from "./Footer"
+import GetStarted from "./GetStarted"
+import Hero from "./Hero"
+import NavBar from "./NavBar"
+import Stats from "./Stats"
+import Testimonials from "./Testimonials"
+import Button from "./Button"
+
+export {
+    Stats,
+    Business,
+    Billing,
+    CardDeal,
+    Testimonials,
+    Button,
+    Clients,
+    CTA,
+    Footer,
+    FeedBackCard,
+    GetStarted,
+    Hero,
+    NavBar,
+}
